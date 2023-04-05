4fdc6e7db421e3332bbd8f68c2767856c8e29519
ArchaicVirus <richardhall1029@gmail.com>
2023-04-05 17:53:27 -0400

    test

--- pushRepo.bat
+++ pushRepo.bat
@@ -8,7 +8,7 @@ echo Committing changes...
 git commit -m "%message%"
 echo.
 echo Pushing to remote repository...
-git push --set-upstream origin master
+git push --set-upstream origin remote
 echo.
 echo Done.
 pause
